16973c9744e6839c403601dca6546c3cb8b66264
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-18 16:12:03 +0200

    *add* [aliments], cicled with for and appended

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo for e con un ciclo while (potete farlo nello stesso file o in due file separati).
+ 
+*/
+
+const list = document.querySelector('ul');
+
+const aliments = [
+    'Pane',
+    'Latte',
+    'Pasta',
+    'Verdure',
+];
+
+
+
+for(let i = 0; i < aliments.length; i++){
+    const item = document.createElement('li')
+    item.append(aliments[i]);
+    list.append(item);
+};
